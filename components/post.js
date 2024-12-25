5225f6b05c2a6e21d66aa7d4705a4a3788156700
07-Vinesh <2006vigneshvicky@gmail.com>
2024-12-25 16:03:52 +0530

    first commit

--- components/post.js
+++ components/post.js
@@ -1,8 +1,9 @@
 "use client"
+import * as React from "react"
 import { useEffect, useState } from "react";
 
 export default function Post({params}) {
-    const id = params.id;
+    const {id} = React.use(params);
     const [post, setPost] = useState(null);
 
     useEffect(() => {
@@ -16,7 +17,7 @@ export default function Post({params}) {
             {post && <main className="container mx-auto px-4 py-6">
                 <h2 className="text-4xl font-bold mb-4">{post.title}</h2>
                 <p className="text-gray-500">Published on {post.created_at_formatted}</p>
-                <img width={300} height={200} src={post.image} alt="Post Image" className="my-4" />
+                <img width={300} height={200} src={post.Image} alt="Post Image" className="my-4" />
                 <p>{post.description}</p>
             </main>
             }
